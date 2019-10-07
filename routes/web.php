4d6f3532e9cc2a59873963c65eaf402d36aaee04
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2019-10-07 17:40:52 -0500

    Scaffold ui and authentication

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* @var \Illuminate\Routing\Router $router */
+$router->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$router->post('login', 'Auth\LoginController@login');
+$router->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+$router->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$router->post('register', 'Auth\RegisterController@register');
+
+$router->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$router->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$router->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$router->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+$router->view('/', 'welcome');
+$router->get('home', 'HomeController@index')->name('home');
